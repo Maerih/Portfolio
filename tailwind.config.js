62513e9d8d15602365065b7b7db9a5b1a9e8ed19
Maerih <onyonka.maeri@sciat.africa>
2024-08-17 13:53:06 +0300

    llsda

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 const { redirect } = require("next/dist/server/api-utils");
 
 module.exports = {
-  base: "Portfolio",
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
